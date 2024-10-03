71cd346924a77e3d00520b474ebfaf20a7401839
Samuele de Tomasi <el3um4s@gmail.com>
2024-10-03 13:08:31 +0200

    Update +page.svelte

--- src/routes/comics/[slug]/+page.svelte
+++ src/routes/comics/[slug]/+page.svelte
@@ -5,7 +5,7 @@
 	import { dev } from '$app/environment';
 	export let data;
 
-	// console.log(data.index);
+	console.log(data);
 </script>
 
 <!-- SEO -->
@@ -13,10 +13,11 @@
 	<title>{data.slug}</title>
 	<meta property="og:type" content="article" />
 	<meta property="og:url" content="{base}{data.url}" />
-	<meta property="og:title" content={data.slug} />
+	<meta property="og:title" content={data.title} />
 	<meta property="og:site_name" content={config.title} />
+	<meta property="og:description" content={data.category} />
 
-	<meta property="og:image" content="{config.repository}/raw/main/src/posts/comics/{data.src}" />
+	<meta property="og:image" content="{config.repository}/raw/main/src/posts/comics{data.src}" />
 
 	<meta property="article:published_time" content="{data.date.substring(0, 10)}T00:00:00+00:00}" />
 
@@ -74,13 +75,13 @@
 				</a>
 			</div>
 			{#if dev}
-				{#await import(/* @vite-ignore */ `/src/posts/comics/${data.src}`) then { default: src }}
+				{#await import(/* @vite-ignore */ `/src/posts/comics${data.src}`) then { default: src }}
 					<img class="comics-image" {src} alt={data.title} loading="lazy" />
 				{/await}
 			{:else}
 				<img
 					class="comics-image"
-					src="{config.repository}/raw/main/src/posts/comics/{data.src}"
+					src="{config.repository}/raw/main/src/posts/comics{data.src}"
 					alt={data.title}
 					loading="lazy"
 				/>
